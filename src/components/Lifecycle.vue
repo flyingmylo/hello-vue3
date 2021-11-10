337622a5e1ecb565fa3854adb31163a031e63793
应博明 <yingbm@isyscore.com>
2021-11-10 19:53:26 +0800

    第一次提交

---
+++ src/components/Lifecycle.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { onMounted, defineProps, watch } from "vue";
+
+const props = defineProps({
+  name: String,
+});
+console.log("kkkk", props);
+// watch(props.name, (value) => {
+//   console.log("llllllifecyle", value);
+// });
+</script>
+
+<template>
+  <input type="text" v-model="name" />
+</template>
